8e75c56048333170a3e8341ef00e762893589595
zeiiv <53347297+zeiiv@users.noreply.github.com>
2025-06-09 12:23:15 +0300

    renable lang switcher

--- _plugins/fix_lang_switcher.rb
+++ _plugins/fix_lang_switcher.rb
@@ -1,4 +1,3 @@
- _plugins/fix_lang_switcher.rb
 Jekyll::Hooks.register :site, :post_write do |site|
   puts ">>> Running fix_lang_switcher.py after Jekyll build..."
   system("python3 assets/py/fix_lang_switcher.py")
